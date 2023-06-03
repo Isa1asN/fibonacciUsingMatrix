a90f2420d2cf40e3884de6e78d6f02edf46ce409
afnoro <135457968+afnoro@users.noreply.github.com>
2023-06-03 21:21:03 +0300

    Update fib_matrix.py

--- fib_matrix.py
+++ fib_matrix.py
@@ -3,26 +3,27 @@ def mult(x,y):
 		a = x[0]*y[0]+x[1]*y[1]
 		b = x[2]*y[0]+x[3]*y[1]
 		return [a,b]
-	a = x[0]*y[0] + x[1]*y[2]
-	b = x[0]*y[1] + x[1]*y[3]
-	c = x[2]*y[0] + x[3]*y[2]
-	d = x[2]*y[1] + x[3]*y[3]
+	a = x[0] * y[0] + x[1] * y[2]
+	b = x[0] * y[1] + x[1] * y[3]
+	c = x[2] * y[0] + x[3] * y[2]
+	d = x[2] * y[1] + x[3] * y[3]
+	return [a, b, c, d]
+
 	return [a,b,c,d]
 
 # Only works for positive powers!
-def matrix_power( x, n ):
-	if ( n == 1 ):
-		return x
-	if ( n%2 == 0 ):
-		return matrix_power( mult(x, x), n//2 )
-	return mult(x, matrix_power( mult(x, x), n//2 ) )
-	
-# fibonacci example:
-A = [1,1,1,0]
-v = [1,0]
+def matrix_power(x, n):
+	if n == 1:
+	return x
+	if n % 2 == 0:
+	return matrix_power(mult(x, x), n // 2)
+	return mult(x, matrix_power(mult(x, x), n // 2))
+
 def main():
-    x = int(input('Enter the number to find the nth fibonacci: '))
-    print (mult(matrix_power(A,x-1),v)[0])
+	A = [1, 1, 1, 0]
+	v = [1, 0]
+	x = int(input('Enter the number to find the nth Fibonacci: '))
+	print(mult(matrix_power(A, x - 1), v)[0])
 
 if __name__=='__main__':
     main()
